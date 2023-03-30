22c2f8fbfcd8f39e89df7ab96e93f8bcee94c295
Sérgio Lima <sergiolimafg3@gmail.com>
2023-03-30 09:50:38 -0300

    resolução do exercicio Form React use state

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,41 @@
+import React, {useState} from 'react';
+
 const App = () => {
+const [nome, setName] = useState('');
+const [sobreNome, setsobreNome] = useState('');
+const [idade, setIdade] = useState('');
+
+const handlerInput = (event: React.ChangeEvent<HTMLInputElement>) => {
+  setName(event.target.value);
+}
+const handlesobreInput = (event: React.ChangeEvent<HTMLInputElement>) => {
+  setsobreNome(event.target.value);
+}
+const handleidadeInput = (event: React.ChangeEvent<HTMLInputElement>) => {
+  setIdade(event.target.value);
+}
+
   return (
     <div>
       <div>
         Nome:
-        <input type="text" />
+        <input type="text" value={nome} onChange={handlerInput} />
       </div>
 
       <div>
         Sobrenome:
-        <input type="text" />
+        <input type="text" value={sobreNome} onChange={handlesobreInput}/>
       </div>
 
       <div>
         Idade:
-        <input type="text" />
+        <input type="text" value={idade} onChange={handleidadeInput} />
       </div>
 
       <hr/>
 
-      Olá ---, tudo bem?<br/>
-      Você tem -- anos.
+      Olá {nome}{sobreNome}, tudo bem?<br/>
+      Você tem {idade} anos.
     </div>
   );
 }
